db9dfb140ce322e9319638ae3775bcee431aedf0
Gu_ZT <administration@ztxy666.cn>
2022-05-29 22:39:04 +0800

    update

--- Packer/GetSponsors.py
+++ Packer/GetSponsors.py
@@ -1,6 +1,6 @@
 import json
 import os
-from typing import List, Dict
+from typing import Dict
 
 from Packer import config_path
 
@@ -11,11 +11,14 @@ sponsors_file = os.path.join(config_path, 'sponsors.json')
 
 def reload():
     try:
-        text = json.dumps(
-            requests.get(
-                "https://afdian.net/api/creator/get-top-sponsors?user_id=b0333d22d78811ecb3cd52540025c377").json()["data"]["list"],
-            ensure_ascii=False,
-            indent=4)
+        text = requests.get(
+            "https://afdian.net/api/creator/get-top-sponsors?user_id=b0333d22d78811ecb3cd52540025c377"
+        ).json()["data"]["list"]
+        x: dict = {}
+        names = [i["name"] for i in text]
+        for i in range(len(names)):
+            x[names[i]] = text[i]["avatar"]
+        text = json.dumps(x, ensure_ascii=False, indent=4)
         if not os.path.isdir(config_path):
             os.mkdir(config_path)
         if not os.path.isfile(sponsors_file):
@@ -29,6 +32,9 @@ def reload():
         return
 
 
-def get_sponsors() -> List[Dict]:
+def get_sponsors() -> Dict:
     with open(sponsors_file, 'r') as f:
         return json.loads(f.read())
+
+
+print(get_sponsors())
